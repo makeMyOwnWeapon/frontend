c7e24d86934879f9f29cda9b084384edbb192cd3
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-26 16:29:36 +0900

    fix: account

--- src/components/main/account.tsx
+++ src/components/main/account.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { Route, Routes, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import WorkBook from '../../pages/workbook';
 import Signup from '../../pages/signup';
+
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || 'default_client_id';
 
 const Account: React.FC = () => {
   const navigate = useNavigate();
+
   useEffect(() => {
     window.google?.accounts.id.initialize({
       client_id: clientId,
@@ -14,22 +17,24 @@ const Account: React.FC = () => {
 
     window.google?.accounts.id.renderButton(
       document.getElementById('signInDiv'),
-      { theme: 'outline', size: 'large' }  // 버튼 옵션
+      { theme: 'outline', size: 'large' }  // 로그인 버튼 옵션
     );
 
     window.google?.accounts.id.prompt(); // 세션 종료 시 자동 로그인 프롬프트
   }, []);
 
-  const handleCredentialResponse = (response: any) => {
-    console.log(response.clientId)
-    console.log(response.client_id)
-    console.log(response.credential)
-    console.log(response.selected_by)
-    // 여기서 response를 사용하여 사용자 로그인 처리
-    navigate('/workbook');
-
-  };        
-  
+  const handleCredentialResponse = async (userToken: string) => {
+    try {
+      const response = await axios.get('http://192.168.0.143:3000/api/member/signin', {
+        headers: {
+          'Authorization': `Bearer ${userToken}`
+        }
+      });
+      console.log('User Data:', response.data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
 
   return (
     <div>
@@ -40,7 +45,6 @@ const Account: React.FC = () => {
 
       <div id="signInDiv"></div> {/* 로그인 버튼이 렌더링될 위치 */}
     </div>
-
   );
 };
 
