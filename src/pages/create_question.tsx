07ee8ace4b983eaf56d9713aceeebc11b0dff752
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-26 13:38:38 +0900

    fff

--- src/pages/create_question.tsx
+++ src/pages/create_question.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import { NavContainer, PageBackGround } from "../styles/Public";
 import NavBar from "../components/public/navbar";
 import ProblemPage from "../components/create/create_question";
-import { Content } from "../styles/MainStyles";
 
 const Create: React.FC = () => {
     return (
         <>
         <NavBar /> 
         <NavContainer> 
-        
         </NavContainer>
-        
         <PageBackGround>
         <ProblemPage/>
         </PageBackGround>
