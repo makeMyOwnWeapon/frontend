c78b15daf5686735cc931b11d5f01a8a5304e583
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-04-26 13:59:58 +0900

    chore:google_login

--- src/components/main/account.tsx
+++ src/components/main/account.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react';
-
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import WorkBook from '../../pages/workbook';
+import Signup from '../../pages/signup';
 const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || 'default_client_id';
 
 const Account: React.FC = () => {
+  const navigate = useNavigate();
   useEffect(() => {
     window.google?.accounts.id.initialize({
       client_id: clientId,
@@ -18,14 +21,26 @@ const Account: React.FC = () => {
   }, []);
 
   const handleCredentialResponse = (response: any) => {
-    console.log('인증 데이터:', response);
+    console.log(response.clientId)
+    console.log(response.client_id)
+    console.log(response.credential)
+    console.log(response.selected_by)
     // 여기서 response를 사용하여 사용자 로그인 처리
-  };
+    navigate('/workbook');
+
+  };        
+  
 
   return (
     <div>
+      <Routes>
+        <Route path="/workbook" element={<WorkBook/>} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+
       <div id="signInDiv"></div> {/* 로그인 버튼이 렌더링될 위치 */}
     </div>
+
   );
 };
 
