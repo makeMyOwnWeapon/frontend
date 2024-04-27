cb5d54c448d8aad133a953cec0e9cfbed79d37a0
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-04-27 21:44:42 +0900

    chore:create_question

--- src/components/create/create_question.tsx
+++ src/components/create/create_question.tsx
@@ -6,72 +6,56 @@ import QuestionComponent from './create_question_component';
 
 const ProblemPage: React.FC = () => {
   const [videoUrl, setVideoUrl] = useState<string>('');
-  const [questionComponents, setQuestionComponents] = useState<{ id: number, expanded: boolean, answers: string[] }[]>([]);
+  const [questionComponents, setQuestionComponents] = useState<{ id: number, expanded: boolean}[]>([]);
+  const [answers, setAnswers] = useState<string[][]>([]);
+
+  const addQuestionComponent = () => {
+    setQuestionComponents([...questionComponents, { id: questionComponents.length, expanded: false }]);
+    setAnswers([...answers, ['', '', '', '', '']]);
+  };
+
+  const updateAnswers = (index: number, newAnswers: string[]) => {
+    setAnswers(answers => {
+      const updatedAnswers = [...answers];
+      updatedAnswers[index] = newAnswers;
+      return updatedAnswers;
+    });
+  };
 
   const toggleExpand = (id: number) => {
-    const updatedComponents = questionComponents.map(component =>
-      component.id === id ? { ...component, expanded: !component.expanded } : component
-    );
-    setQuestionComponents(updatedComponents);
-  };
-  
-  const handleUpdateQuestion = (id: number, newAnswers: string[]) => {
-    const updatedComponents = questionComponents.map(component =>
-      component.id === id ? { ...component, answers: newAnswers } : component
-    );
-    setQuestionComponents(updatedComponents);
+    const newComponents = questionComponents.map(component => {
+      if (component.id === id) {
+        return { ...component, expanded: !component.expanded };
+      }
+      return component;
+    });
+    setQuestionComponents(newComponents);
   };
 
   const deleteQuestion = (id: number) => {
-    const updatedComponents = questionComponents.filter(component => component.id !== id);
-    setQuestionComponents(updatedComponents);
-  };
-
-  const addQuestionComponent = () => {
-    const newComponent = {
-      id: Date.now(),
-      expanded: false,
-      answers: []
-    };
-    setQuestionComponents([...questionComponents, newComponent]);
+    const filteredComponents = questionComponents.filter(component => component.id !== id);
+    setQuestionComponents(filteredComponents.map((component, index) => ({
+      ...component,
+      id: index,
+      expanded: component.expanded
+    })));
+    setAnswers(answers.filter((_, index) => index !== id));
   };
 
   const postData = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     try {
-      questionComponents.forEach(component => {
-        console.log(`ID: ${component.id}, Answers: ${component.answers}`);
-      });
-
-      const response = await fetch('http://192.168.0.143:3000/api/member/registration', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          videoUrl: videoUrl,
-          questionComponents: questionComponents,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const responseData = await response.json();
-      console.log('서버 응답:', responseData);
+      console.log(answers);
+      // API call can be made here
     } catch (error) {
       console.error('오류 발생:', error);
     }
   };
-  
+
   return (
     <>
       <Form>
-        <StyledText>
-          나만의 문제 만들기
-        </StyledText>
+        <StyledText>나만의 문제 만들기</StyledText>
         <InputContainer>
           <Input type="text" placeholder="동영상 URL" value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} />
           <VideoThumbnail imageUrl={videoUrl}/> 
@@ -81,16 +65,17 @@ const ProblemPage: React.FC = () => {
             <QuestionComponent
               id={component.id}
               expand={component.expanded}
-              onToggle={() => toggleExpand(component.id)}
-              onDelete={() => deleteQuestion(component.id)}
-              onUpdate={(id, newAnswers) => handleUpdateQuestion(id, newAnswers)}
+              onToggle={toggleExpand}
+              onDelete={deleteQuestion}
+              answers={answers[index]}
+              updateAnswer={(newAnswers) => updateAnswers(index, newAnswers)}
             />
           </div>
         ))}
         <NameGeneratorButton type="button" onClick={addQuestionComponent}>문제 추가</NameGeneratorButton>
       </Form>
       <Form onSubmit={postData}>
-          <NameGeneratorButton type="submit">제출하기</NameGeneratorButton>
+        <NameGeneratorButton type="submit">제출하기 버튼</NameGeneratorButton>
       </Form>
     </>
   );
