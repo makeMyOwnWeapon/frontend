3666eddf9bd5739b785c96c25c9773b23df05e60
kts5927 <154499036+kts5927@users.noreply.github.com>
2024-05-01 23:47:50 +0900

    chore:Thumbnail

--- src/components/public/url_to_image.tsx
+++ src/components/public/url_to_image.tsx
@@ -1,28 +1,35 @@
 import React from 'react';
-import inflearn from '../../images/inflearn.png'
-import krafton from '../../images/krafton.png'
-import defaultThumbnail from '../../images/default_thumbnail.jpeg'
+import inflearn from '../../images/inflearn.png';
+import krafton from '../../images/krafton.png';
+import defaultThumbnail from '../../images/default_thumbnail.png';
 import { ImageWithShadow } from '../../styles/Public';
 
 interface Props {
     imageUrl: string;
-  }
+}
 
 const VideoThumbnail: React.FC<Props> = ({ imageUrl }) => {
     
-  const thumbnailMappings :{ [key: string]: string } = {
+  const thumbnailMappings: { [key: string]: string } = {
     'inflearn.com': inflearn,
     'krafton.com': krafton,
-    // 도메인과 썸네일을 추가하여 확장시킬 수 있음.
+    // 추가 도메인 및 썸네일 확장 가능
   };
 
-  const getVideoThumbnail = (url:string) => {
-    const domain = url.startsWith('https') ? new URL(url).hostname.replace('www.', '') : '';
-    if (domain === null){
-      return defaultThumbnail;
+  const getVideoThumbnail = (url: string) => {
+    try {
+      const parsedUrl = new URL(url);
+      const domain = parsedUrl.hostname.replace('www.', '');
+
+      if (domain === 'youtube.com') {
+        const videoId = domain === 'youtube.com' ? parsedUrl.searchParams.get('v') : url.split('/').pop();
+        return `https://img.youtube.com/vi/${videoId}/0.jpg`; // 유튜브 썸네일 URL
+      }
+
+      return thumbnailMappings[domain] || defaultThumbnail;
+    } catch (error) {
+      return defaultThumbnail; // 유효하지 않은 URL의 경우 기본 썸네일
     }
-    const thumbnail = thumbnailMappings[domain];
-    return thumbnail || defaultThumbnail;
   };
 
   return (
