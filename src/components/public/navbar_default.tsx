78316d02ba6500cbbdabbc1050f07dc565430dea
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-03 16:37:38 +0900

    fix: navbar

--- src/components/public/navbar_default.tsx
+++ src/components/public/navbar_default.tsx
@@ -29,9 +29,9 @@ const NavBar: React.FC = () => {
   return (
     <NavBarContainer>
       <NavLink href="" onClick={goHome}>Home</NavLink>
-      <NavLink href="" onClick={()=>startNavigate("workbook")}>workbook</NavLink>
+      <NavLink href="" onClick={()=>startNavigate("workbook")}>Workbook</NavLink>
       <NavLink href="" onClick={()=>startNavigate("create")}>Create</NavLink>
-      <NavLink href="" onClick={()=>startNavigate("video")}>영상촬영</NavLink>
+      <NavLink href="" onClick={()=>startNavigate("video")}>Videography</NavLink>
       <NavLink href="" onClick={handleLogout}>Logout</NavLink>
     </NavBarContainer>
   );
