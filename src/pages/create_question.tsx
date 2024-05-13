8572f7640e909f71e2620b9409f90a5097357ea0
UkdongKim <edisen315@gmail.com>
2024-05-14 00:57:19 +0900

    문제집 만들기 초안 작성

--- src/pages/create_question.tsx
+++ src/pages/create_question.tsx
@@ -1,20 +1,52 @@
 import React from "react";
-import { PageBackGround } from "../styles/Public";
+import { Container, PageBackGround } from "../styles/Public";
 import ProblemPage from "../components/create/create_question";
 import NavBar from "../components/public/navbar_default";
 import { useNavigate } from "react-router-dom";
+import BackgroundAnimation from "../styles/Background";
+import NaviSection from "../components/new_components/NaviSection";
+import styled from "styled-components";
+import SidebarOptions from "../components/board/select_option";
 
 const Create: React.FC = () => {
     const navigate = useNavigate();
-    
+    const currentMenuName = '문제집 만들기'
+
     return (
-        <>
-            <NavBar /> 
-            <PageBackGround>
-                <ProblemPage navigate={navigate}/>
-            </PageBackGround>
-        </>
+
+    <BackgroundAnimation>
+        <Container>
+          <NaviSection currentMenuName = {currentMenuName}></NaviSection>
+              <InnerContentSection>
+                <Side>
+                    <SidebarOptions/>
+                </Side>
+
+                <ProblemPage navigate={navigate}/> 
+
+              </InnerContentSection>
+        </Container>
+     </BackgroundAnimation>
+        
+
+
+
+         
+        
     );
 };
 
 export default Create;
+
+const InnerContentSection = styled.div`
+    display: flex;
+    height: 85%;
+`
+export const Side = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+  /* border: 1px solid red; */
+  width: 20%;
+`;
