b938b62d2ddd1ae3877fdd0dafa474739ab6e486
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-15 00:21:36 +0900

    fix: quizSetInfo

--- src/components/quizSetDetail/quizSetInfo.tsx
+++ src/components/quizSetDetail/quizSetInfo.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
-import { useLocation } from "react-router-dom";
-import { Form } from '../../styles/CreateQuestion';
-
-import { PublicSliderContainer, PublicTextContainer, PublicQuestionContainer } from "../../styles/Public";
+import Modal from "react-modal";
+import { PublicSliderContainer, PublicQuestionContainer } from "../../styles/Public";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../../styles/css/slick.css';
@@ -21,69 +19,232 @@ interface Choice {
 }
 
 interface Question_ {
-    choice: Choice[];
+    choices?: Choice[];
     commentary: string;
     instruction: string;
     popupTime: string;
 }
 
-const QuizSetInfo = ({ videoUrl, quizSetId }: QuestionComponentProps) => {
-    const location = useLocation();
-    const { subLectureTitle, quizSetTitle, memberNickname } = location.state || {};
-    const [data, setData] = useState<Question_[] | undefined>(undefined);
-        
+const formatTime = (seconds: number) => {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+
+    if (h > 0) {
+        return `${h}시간 ${m}분 ${s}초`;
+    } else if (m > 0) {
+        return `${m}분 ${s}초`;
+    } else {
+        return `${s}초`;
+    }
+};
+
+const QuizSetInfo = ({ quizSetId }: QuestionComponentProps) => {
+    const [data, setData] = useState<Question_[]>([]);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [currentCommentary, setCurrentCommentary] = useState("");
+
     useEffect(() => {
         const fetchData = async () => {
-            const response = await request('GET', `/api/quizsets/${quizSetId}/quizzes`);
-            setData(response.data);
-            console.log(response.data);
+            try {
+                const response = await request('GET', `/api/quizsets/${quizSetId}/quizzes`);
+                if (response.data) {
+                    setData(response.data);
+                    console.log(response.data);
+                }
+            } catch (error) {
+                console.error("Error fetching quiz data:", error);
+            }
         };
-    
+
         fetchData();
     }, [quizSetId]);
 
     const settings = {
         dots: true,
-        infinite: data && data.length > 1,
+        infinite: data.length > 1,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        prevArrow: <CustomPrevArrow />,
+        nextArrow: <CustomNextArrow />
     };
 
-    return (
-        <Form>
-            <PublicSliderContainer>
-                <Slider className="custom-slider" {...settings}>
-                    {data && data.map((question, index) => (
-                        <PublicQuestionContainer key={index}>
-                            <PublicTextContainer>문제집명: {quizSetTitle}</PublicTextContainer>
-                            <PublicTextContainer>소강의명: {subLectureTitle}</PublicTextContainer>
-                            <PublicTextContainer>작성자: {memberNickname}</PublicTextContainer>
+    const openModal = (commentary: string) => {
+        setCurrentCommentary(commentary);
+        setModalIsOpen(true);
+    };
 
-                            <Question key={index}>
-                                {question.choice && question.choice.map((choice, choiceIndex) => (
-                                    <PublicTextContainer key={choiceIndex}>{choice.content}</PublicTextContainer>
-                                ))}
-                            </Question>
-                        </PublicQuestionContainer>
-                    ))}
-                </Slider>
-            </PublicSliderContainer>
-        </Form>
+    const closeModal = () => {
+        setModalIsOpen(false);
+        setCurrentCommentary("");
+    };
+
+    if (data.length === 0) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <PublicSliderContainer>
+            <StyledSlider {...settings}>
+                {data.map((question, index) => (
+                    <PublicQuestionContainer key={index}>
+                        <Question>
+                            <QuizSetInfoTextContainer>
+                                <Instruction>{`${index + 1}. ${question.instruction}`}</Instruction>
+                         
+                                {question.choices && question.choices.length === 4 ? (
+                                    <Choices>
+                                        <QuestionType>객관식 문제</QuestionType>
+                                        {question.choices.map((choice, idx) => (
+                                            <Choice key={choice.choiceId}>{`${idx + 1}) ${choice.content}`}</Choice>
+                                        ))}
+                                        
+                                    </Choices>
+                                ) : <QuestionType>주관식 문제</QuestionType>}
+                                <PopupTime>{`Popup Time: ${formatTime(Number(question.popupTime))}`}</PopupTime>
+                                <CommentaryButton onClick={() => openModal(question.commentary)}>해설보기</CommentaryButton>
+                            </QuizSetInfoTextContainer>
+                        </Question>
+                    </PublicQuestionContainer>
+                ))}
+            </StyledSlider>
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                style={customStyles}
+                contentLabel="Commentary Modal"
+            >
+                <h2>해설</h2>
+                <p>{currentCommentary}</p>
+                <CloseButton onClick={closeModal}>닫기</CloseButton>
+            </Modal>
+        </PublicSliderContainer>
     );
 };
 
 export default QuizSetInfo;
 
-
 const Question = styled.div`
-  flex: 1; /* 콘텐츠 영역이 남은 공간을 차지 */
+  flex: 1;
   display: flex;
   flex-wrap: wrap;
-  border: 1px solid #ccc; /* 테두리 스타일 지정 */
-  border-radius: 5px; /* 테두리 모서리를 둥글게 만듦 */
-  padding: 10px; /* 내부 여백 추가 */
-  margin: 10px; /* 외부 패딩 추가 */
-  flex-direction:column;
-  min-height: 400px;
+  border-radius: 5px;
+  padding: 10px;
+  flex-direction: column;
+  min-height: 350px;
 `;
+
+const QuizSetInfoTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding-top: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  background-color: rgba(249, 249, 249, 0.7);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  position: relative;
+`;
+
+const Instruction = styled.div`
+  font-size: 1.3em;
+  margin-bottom: 20px;
+`;
+
+const PopupTime = styled.div`
+  font-size: 1em;
+  margin-top: 10px;
+  margin-left: 5px;
+  margin-bottom: 5px;
+`;
+
+const Choices = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Choice = styled.div`
+  margin-left: 5px;
+  margin-bottom: 15px;
+`;
+
+const QuestionType = styled.div`
+  font-size: 1em;
+  font-weight: bold;
+  color: #007bff;
+  margin-left: 5px;
+  margin-bottom: 20px;
+`;
+
+const CommentaryButton = styled.button`
+  margin-top: 20px;
+  margin-left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 10px 15px;
+  background-color: rgba(104, 178, 253, 0.7);
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #82b8f2;
+  }
+`;
+
+const CloseButton = styled.button`
+  display: block;
+  margin: 20px auto;
+  padding: 10px 20px;
+  background-color: rgba(104, 178, 253, 0.7);
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    background-color: #82b8f2;
+  }
+`;
+
+const ArrowButton = styled.div`
+  display: block;
+  padding: 10px;
+  border-radius: 50%;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+const CustomPrevArrow = (props: any) => (
+  <ArrowButton
+    style={{ ...props.style, position: 'absolute', bottom: '1px', left: '1px' }}
+    onClick={props.onClick}
+  >
+    &lt;
+  </ArrowButton>
+);
+
+const CustomNextArrow = (props: any) => (
+  <ArrowButton
+    style={{ ...props.style, position: 'absolute', bottom: '1px', right: '1px' }}
+    onClick={props.onClick}
+  >
+    &gt;
+  </ArrowButton>
+);
+
+const StyledSlider = styled(Slider)`
+  .slick-dots {
+    bottom: -90%;
+  }
+`;
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    maxWidth: '600px',
+  }
+};
--- src/pages/quizSetDetail.tsx
+++ src/pages/quizSetDetail.tsx
@@ -1,18 +1,77 @@
 import React from 'react';
 import QuestionInfoComponent from '../components/quizSetDetail/quizSetInfo';
-import { useLocation,  useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
+import BackgroundAnimation from '../components/public/BackgroundAnimation';
+import Container from '../styles/publicStyleComponents/Container';
+import NaviSection from '../styles/publicStyleComponents/NaviSection';
+import styled from 'styled-components';
+import Side from '../styles/publicStyleComponents/Side';
+import ToastModal from '../components/public/toastModal';
 
 const QuestionInfo = () => {
   const location = useLocation();
   const { quizSetId } = useParams();
   const { subLectureUrl } = location.state || { subLectureUrl: "" };
-  
-  return (
-    <div>
+  const currentMenuName = '문제집 조회';
+  const { subLectureTitle, quizSetTitle, memberNickname } = location.state || {};
 
-        {quizSetId && <QuestionInfoComponent videoUrl={subLectureUrl} quizSetId={quizSetId} />}
-    </div>
+  return (
+    <BackgroundAnimation>
+      <Container>
+        <NaviSection currentMenuName={currentMenuName} />
+        <InnerContentSection>
+          <Side>
+            <ToastModal />
+          </Side>
+          <Main>
+            <Title>{quizSetTitle}</Title>
+            <Subtitle>소강의명: {subLectureTitle}</Subtitle>
+            <Author>작성자: {memberNickname}</Author>
+            {quizSetId && <QuestionInfoComponent videoUrl={subLectureUrl} quizSetId={quizSetId} />}
+          </Main>
+        </InnerContentSection>
+      </Container>
+    </BackgroundAnimation>
   );
 };
 
 export default QuestionInfo;
+
+const InnerContentSection = styled.div`
+  display: flex;
+  height: 85%;
+`;
+
+const Main = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-top: 10px;
+  padding: 20px;
+  border: 1px solid #ccc;
+`;
+
+const Title = styled.div`
+  font-size: 2em;
+  font-weight: bold;
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-bottom: 40px;
+  color: #333;
+`;
+
+const Subtitle = styled.div`
+  font-size: 1.4em;
+  margin-left: 10px;
+  margin-bottom: 20px;
+  color: #555;
+`;
+
+const Author = styled.div`
+  margin-left: 10px;  
+  font-size: 1.1em;
+  color: #777;
+  margin-bottom: -80px;
+`;
