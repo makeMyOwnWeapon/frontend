5c06243cd797e58920fd1e98684c39fb57c90dc5
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-05-15 16:18:50 +0900

    add: Swal

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main';
 import Signup from './pages/signup';
